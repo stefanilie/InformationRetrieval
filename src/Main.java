2e00fe0eb4d066298719b129c47f1e59fcffcebb
Stefan Ilie <ilie.stefan92@yahoo.com>
2017-05-08 23:16:30 +0300

    total refactor

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,78 @@
  */
 
 import org.apache.lucene.*;
+
+import java.io.IOException;
 import java.util.Scanner;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.queryparser.classic.QueryParser;
+
 
 
 public class Main {
-    public static void main(String[] args){
+    
+    public static void main(String[] args) throws ParseException {
         System.out.println("Hi mane, I'm using lucene!");
         Scanner scan = new Scanner(System.in);
 
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        Directory index = new RAMDirectory();
+
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+
+        try {
+
+
+        IndexWriter w = new IndexWriter(index, config);
+
+        addDoc(w, "Lucene in Action", "193398817");
+        addDoc(w, "Lucene for Dummies", "55320055Z");
+        addDoc(w, "Managing Gigabytes", "55063554A");
+        addDoc(w, "The Art of Computer Science", "9900333X");
+
+        } catch (IOException e) {
+            System.out.println("moloz la tava");
+        }
+
+        //Query
+
+        String querystr = args.length > 0 ? args[0] : "lucene";
+        Query q = new QueryParser("title", analyzer).parse(querystr);
+
+        //Searching
+        try {
+            int hitsPerPage = 10;
+            IndexReader reader = DirectoryReader.open(index);
+            IndexSearcher searcher = new IndexSearcher(reader);
+            TopDocs docs = searcher.search(q, hitsPerPage);
+            ScoreDoc[] hits = docs.scoreDocs;
+        } catch (IOException e) {
+            System.out.println("moloz la tava");
+        }
+
     }
+
+    private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+        Document doc = new Document();
+        doc.add(new TextField("title", title, Field.Store.YES));
+        doc.add(new StringField("isbn", isbn, Field.Store.YES));
+        w.addDocument(doc);
+    }
+
 }

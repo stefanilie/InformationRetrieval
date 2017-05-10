64f3f256d3ab899d1a2f276d6864855bd089fcad
Stefan Ilie <ilie.stefan92@yahoo.com>
2017-05-10 08:17:03 +0300

    lab work

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@
 
 import org.apache.lucene.*;
 
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -25,9 +29,16 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.queryparser.classic.QueryParser;
 
+import java.io.IOException;
+
 
 
 public class Main {
+
+    private static final String FILENAME = "/Users/stefan.ilie/IdeaProjects/RegasireaInformatiei/ciorba.txt";
+    private static final String FILENAME2 = "/Users/stefan.ilie/IdeaProjects/RegasireaInformatiei/ciorba.html";
+    private static final String FILENAME3 = "/Users/stefan.ilie/IdeaProjects/RegasireaInformatiei/test2.txt";
+
 
     public static String parseString(String searchWord) {
         if (searchWord.matches(".*[ăâșțîĂÂȘȚÎ].*")){
@@ -65,6 +76,62 @@ public class Main {
         return searchWord;
     }
 
+    public static boolean checkSearchWord(String searchWord){
+        if (Arrays.asList("si", "in", "la", "și", "în", "sa", "să").contains(searchWord)){
+            return false;
+        }
+        return true;
+    }
+
+
+    public static void loadFile(IndexWriter w){
+
+        ArrayList<String> paths = new ArrayList<String>();
+        paths.add(FILENAME);
+        paths.add(FILENAME2);
+        paths.add(FILENAME3);
+        for (String path : paths) {
+
+            BufferedReader br = null;
+            FileReader fr = null;
+
+            try {
+
+                fr = new FileReader(path);
+                br = new BufferedReader(fr);
+
+                String sCurrentLine;
+
+                br = new BufferedReader(new FileReader(path));
+
+                while ((sCurrentLine = br.readLine()) != null) {
+                    addDoc(w, sCurrentLine, path);
+                }
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            } finally {
+
+                try {
+
+                    if (br != null)
+                        br.close();
+
+                    if (fr != null)
+                        fr.close();
+
+                } catch (IOException ex) {
+
+                    ex.printStackTrace();
+
+                }
+
+            }
+        }
+    }
+
     public static void main(String[] args) throws ParseException {
         Scanner scan = new Scanner(System.in);
 
@@ -81,42 +148,48 @@ public class Main {
 
         IndexWriter w = new IndexWriter(index, config);
 
-        addDoc(w, "Lucene in Action Făcâturî");
-        addDoc(w, "Lucene for Dummies");
-        addDoc(w, "Managing Gigabytes");
-        addDoc(w, "The Art of Computer Șcience");
+        loadFile(w);
+//
+//        addDoc(w, "Lucene in Action Făcâturî");
+//        addDoc(w, "Lucene for Dummies");
+//        addDoc(w, "Managing Gigabytes");
+//        addDoc(w, "The Art of Computer Șcience");
         w.close();
 
         } catch (IOException e) {
             System.out.println("moloz la tava:\n"+e.getMessage());
         }
 
-        //Query
-        Query q = new QueryParser("parsedString", analyzer).parse(parseString(searchWord));
+        if (checkSearchWord(searchWord)){
+            //Query
+            Query q = new QueryParser("parsedString", analyzer).parse(parseString(searchWord));
 
-        //Searching
-        try {
-            int hitsPerPage = 10;
-            IndexReader reader = DirectoryReader.open(index);
-            IndexSearcher searcher = new IndexSearcher(reader);
-            TopDocs docs = searcher.search(q, hitsPerPage);
-            ScoreDoc[] hits = docs.scoreDocs;
+            //Searching
+            try {
+                int hitsPerPage = 10;
+                IndexReader reader = DirectoryReader.open(index);
+                IndexSearcher searcher = new IndexSearcher(reader);
+                TopDocs docs = searcher.search(q, hitsPerPage);
+                ScoreDoc[] hits = docs.scoreDocs;
 
-            System.out.println("Found " + hits.length + " hits.");
-            for(int i=0;i<hits.length;++i) {
-                int docId = hits[i].doc;
-                Document d = searcher.doc(docId);
-                System.out.println((i + 1) + ". " + d.get("text"));
+                System.out.println("\n"+"Found " + hits.length + " hits.");
+                for(int i=0;i<hits.length;++i) {
+                    int docId = hits[i].doc;
+                    Document d = searcher.doc(docId);
+                    System.out.println("\n-------\n"+d.get("path")+"\n" + (i + 1) + ". " +d.get("text"));
+                }
+            } catch (IOException e) {
+                System.out.println("2moloz la tava:\n"+e.getMessage());
             }
-        } catch (IOException e) {
-            System.out.println("2moloz la tava:\n"+e.getMessage());
+
         }
     }
 
-    private static void addDoc(IndexWriter w, String text) throws IOException {
+    private static void addDoc(IndexWriter w, String text, String filePath) throws IOException {
         Document doc = new Document();
         doc.add(new TextField("text", text, Field.Store.YES));
         doc.add(new TextField("parsedString", parseString(text), Field.Store.YES));
+        doc.add(new TextField("path", filePath, Field.Store.YES));
         w.addDocument(doc);
     }
 }
